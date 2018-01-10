a5841598dc44d8921470668fe077b6756a447845
bogdan2442 <rusubogdan77@gmail.com>
2018-01-10 12:33:58 +0200

    added displacement on X axis based on angle to the StateTemplatev2

--- ai/state_template.py
+++ ai/state_template.py
@@ -1,5 +1,6 @@
 from sim.simulation import Simulation
-from numpy import array
+from numpy import array, sign
+from math import cos, pi
 
 
 class StateTemplate:
@@ -64,17 +65,21 @@ class StateTemplatev2:
     def __init__(self, sim: Simulation):
         self.foosmans = []
         self.rev_foosmans = []
-
+        self._rods_initial_postion = []
+        self._rev_rods_initial_postion = []
         table_length = sim.table_info.length
-        self.foosman_length = sim.table_info.foosman_size[1]
+        self.foosman_width = sim.table_info.foosman_size[1]
+        self.foosman_height = sim.table_info.foosman_size[2]
         for rod in sim.table_info.rods:
             _, x, foo_count, foo_dist, foo_offset = rod
             self.foosmans.append([x,
                                   self.get_foosmans_positions(foo_count, foo_dist)])
+            self._rods_initial_postion.append(x)
             self.rev_foosmans.insert(0, [table_length - x,
                                          self.get_foosmans_positions(foo_count,
                                                                      foo_dist)
                                          ])
+            self._rev_rods_initial_postion.insert(0, table_length - x)
         self.max_position = complex(table_length, 1)
 
     @staticmethod
@@ -82,7 +87,7 @@ class StateTemplatev2:
         return c.real, c.imag
 
     def get_foosmans_positions(self, foo_count, foo_dist):
-        half_foosman_width = self.foosman_length / 2
+        half_foosman_width = self.foosman_width / 2
         first_foo = (1 - foo_dist * (foo_count - 1)) / 2
         weight_centers = [first_foo + i * foo_dist for i in range(foo_count)]
         return array([i + j * half_foosman_width
@@ -102,17 +107,48 @@ class StateTemplatev2:
                 possible_offset = min(1 - rod[1][-1], offset)
                 rod[1] += possible_offset
 
+    def __get_offset_by_angle(self, angle):
+        # angle is between -1 and 1
+        # transform from [-90, 90] (or [-pi / 2, pi / 2]) to [0, 180] (or [0, pi])
+        # return a distance to move rod on X axis (simulating angle of rod)
+        # i.e the length of adjacent right with the angle (self.foosman_height is the hypotenuse)
+        angle = ((angle + 1) / 2) * pi
+        cos_res = -cos(angle)
+        if abs(cos_res) < 0.0001:
+            cos_res = 0
+        return cos_res * self.foosman_height
+
+    def apply_angles(self, angles, rods, initial_rods):
+        # angles a list of angles between [-1, 1]
+        # -1 meaning max angle to my goal
+        # 1 meaning max angle to opponent goal
+        assert len(angles) == len(rods)
+        for angle, rod, init_rod in zip(angles, rods, initial_rods):
+            rod[0] += self.__get_offset_by_angle(angle)
+            diff = rod[0] - init_rod
+            if abs(diff) > self.foosman_height:
+                # there is a exceeded boundary
+                # stick to the boundary
+                rod[0] = init_rod + sign(diff) * self.foosman_height
+
     def reset(self):
-        for foosman in self.foosmans:
+        print("Reset called")
+        print("Before\nfoosman = {}\nrev_foosman = {}".format(self.foosmans, self.rev_foosmans))
+        for foosman, init_rod_x in zip(self.foosmans, self._rods_initial_postion):
             # check side distance
             # must be 0 if rods are in initial position
             offset = (1 - foosman[1][-1] - foosman[1][0]) / 2
             foosman[1] += offset
-        for rev_foosman in self.rev_foosmans:
+            foosman[0] = init_rod_x
+        for rev_foosman, rev_init_rod_x in zip(self.rev_foosmans,
+                                               self._rev_rods_initial_postion):
             # check side distance
             # must be 0 if rods are in initial position
             offset = (1 - rev_foosman[1][-1] - rev_foosman[1][0]) / 2
             rev_foosman[1] += offset
+            rev_foosman[0] = rev_init_rod_x
+        print("After\nfoosman = {}\nrev_foosman = {}".format(self.foosmans, self.rev_foosmans))
+        exit(1)
 
     def get_states_from_sim(self, sim: Simulation):
         # TODO: this is not properly reversed.
@@ -130,31 +166,36 @@ class StateTemplatev2:
 
         # rods for player 1
         def unpack(p):
-            return p[0][0], p[0][1], p[1][0], p[1][1]
+            return p[0][1], p[1][1]
 
         self.apply_offset([rod[0][0] for rod in sim.state.rods],
                           self.foosmans)
-
+        self.apply_angles([rod[1][0] for rod in sim.state.rods],
+                          self.foosmans,
+                          self._rods_initial_postion)
         for i, ai_rod in zip(sim.state.rods, self.foosmans):
             state_1.append(ai_rod[0])
             state_1.extend(ai_rod[1])
-            state_1.extend(unpack(i)[1:])
+            state_1.extend(unpack(i))
 
         # rods for player 2
         # both angles and offsets (incl velocities) must be reversed
         self.apply_offset([1 - rod[0][0] for rod in reversed(sim.state.rods)],
                           self.rev_foosmans)
+        self.apply_angles([-rod[1][0] for rod in sim.state.rods],
+                          self.rev_foosmans,
+                          self._rev_rods_initial_postion)
 
         def neg_unpack(state_rod):
             (offset, offset_vel), (angle, angle_vel) = state_rod
             # offset is 0 - 1
             # angle is -1 - 1
             # velocities must be negated
-            return 1 - offset, -offset_vel, -angle, -angle_vel
+            return -offset_vel, -angle_vel
 
         for i, ai_rod in zip(reversed(sim.state.rods), self.rev_foosmans):
             state_2.append(ai_rod[0])
             state_2.extend(ai_rod[1])
-            state_2.extend(neg_unpack(i)[1:])
+            state_2.extend(neg_unpack(i))
 
         return state_1, state_2
