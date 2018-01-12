9c04036ed6fffffabaae2834fdf1d232a0982dca
bogdan2442 <rusubogdan77@gmail.com>
2018-01-12 21:01:24 +0200

    implement toggle for manager

--- ai/manager.py
+++ ai/manager.py
@@ -6,7 +6,6 @@ from ai.state_template import StateTemplate
 class Manager:
     """
     A class that will handle flow of states to the network
-    TODO: toggle to switch between update, non-update
     """
     def __init__(self, brain: AI,
                  sim: Simulation,
@@ -16,8 +15,20 @@ class Manager:
         self.state_template = state_template
         # will preserve (reward, state) until it will be passed to AI update
         self.backup = None
+        self.on_make_action = self._make_action_for_both_players
+        self.on_update = self._update
 
-    def _make_action(self, side: int):
+    def toggle(self, active):
+        # if active is true then made updates on network
+        # else, simple predict actions
+        if active:
+            self.on_make_action = self._make_action_for_both_players
+            self.on_update = self._update
+        else:
+            self.on_make_action = self._make_actions
+            self.on_update = lambda: None
+
+    def _get_single_player_action(self, side: int):
         # player on the side 'side' make a move
         # based on actions taken, update sim
         # return a pair of state, reward
@@ -28,16 +39,36 @@ class Manager:
         reward -= self.sim.get_current_reward(side)
         return self.state_template.get_state_from_sim_for_player(side, self.sim), reward
 
-    def make_action_for_both_players(self):
+    def _make_action_for_both_players(self):
+        # this version is for update
         new_states = [None, None]
         rewards = [None, None]
-        new_states[0], rewards[0] = self._make_action(0)
-        new_states[1], rewards[1] = self._make_action(1)
+        new_states[0], rewards[0] = self._get_single_player_action(0)
+        new_states[1], rewards[1] = self._get_single_player_action(1)
         self.backup = (rewards, new_states)
-        return rewards, new_states
 
-    def update(self):
+    def _make_actions(self):
+        # this version is for version without update
+        # we have a state - give it to the first player and apply his actions
+        state = self.state_template.get_state_from_sim_for_player(0, self.sim)
+        for input in self.brain.predict_action(state, self.brain.multiple_actions):
+            self.sim.apply_inputs(0, input)
+        # after first player made his action, now, new state pass to the second player
+        # and apply his actions
+        state = self.state_template.get_state_from_sim_for_player(1, self.sim)
+        for input in self.brain.predict_action(state, self.brain.multiple_actions):
+            self.sim.apply_inputs(1, input)
+
+    def _update(self):
         if self.backup is not None:
             self.brain.update(*self.backup)
             del self.backup
+
+    def make_action(self):
+        # interface for make action (toggle will switch between semantic of this function)
+        self.on_make_action()
+
+    def update(self):
+        # interface for update (toggle will switch between semantic of this function)
+        self.on_update()
 
